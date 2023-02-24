c9ff0183b5eab7f0072a1264e232fa9251c6cba6
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-02-24 18:10:46 +0530

    added instructions api

--- approve/router/approveRouter.js
+++ approve/router/approveRouter.js
@@ -53,4 +53,16 @@ router.get('/get-allowance', async (req, res) => {
     });
 })
 
+router.get('/instructions', (req, res) => {
+    res.status(200).send([
+        `1. Go to <a href="https://app.uniswap.org/#/swap" target="_blank">Uniswap</a>`,
+        "2. Switch to the Polygon network. The beta version only works on Polygon.",
+        "3. Swap any ERC20 token to any other ERC20 token. Do not select MATIC as the from or to token. We are working on native token swaps and it should be live soon",
+        "4. Select your from token as the gas token in the option ",
+        "5. If you are asked to approve the token, click on the button and sign the message on Metamask. This will approve your token for the swap without you needing to pay any MATIC",
+        "6. Now click on Swap and confirm. You will need to sign the message on Metamask. Once again, no MATIC will be deducted from your wallet.",
+        "7. Congratulations! You've successfully swapped on Uniswap by paying gas fees in an ERC20 token."
+    ])
+})
+
 module.exports = router;
